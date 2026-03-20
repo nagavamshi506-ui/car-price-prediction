8bb8d1b2aaccda3e4aa26fe0a6275e894c717b77
nagavamshi506-ui <nagavamshi506@gmail.com>
2026-03-20 14:46:13 +0530

    finial fix

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import os
 
 # load model and columns
-model = pickle.load(open("model.pkl", "rb"))
-X_columns = pickle.load(open("columns.pkl", "rb"))   # save this from training
-
+BASE_DIR=os.path.dirname(os.path.abspath(__file__))
+model_path=os.path.join(BASE_DIR,'model.pkl')
+columns_path=os.path.join(BASE_DIR,"columns.pkl")
+with open(model_path,"rb") as f:
+    model = pickle.load(f)
+with open(columns_path,"rb") as f:
+    X_columns = pickle.load(f)
 st.title("🚗 Car Price Prediction App")
 
 # INPUTS (UI instead of input())
